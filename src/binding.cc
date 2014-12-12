e7d44ae30532ea016f57d84fccbfe05ef4c3793a
Paul Dorn <pauldorn@gmail.com>
2014-12-12 15:21:18 -0500

    Reduce warnings about conversions

--- src/binding.cc
+++ src/binding.cc
@@ -5,127 +5,7 @@
 #include <string.h>
 
 #ifdef _WIN32
-/*
-void ListIpAddresses(IpAddresses& ipAddrs)
-{
-  IP_ADAPTER_ADDRESSES* adapter_addresses(NULL);
-  IP_ADAPTER_ADDRESSES* adapter(NULL);
 
-  // Start with a 16 KB buffer and resize if needed -
-  // multiple attempts in case interfaces change while
-  // we are in the middle of querying them.
-  DWORD adapter_addresses_buffer_size = 16 * KB;
-  for (int attempts = 0; attempts != 3; ++attempts)
-  {
-    adapter_addresses = (IP_ADAPTER_ADDRESSES*)malloc(adapter_addresses_buffer_size);
-    assert(adapter_addresses);
-
-    DWORD error = ::GetAdaptersAddresses(
-      AF_UNSPEC,
-      GAA_FLAG_SKIP_ANYCAST |
-        GAA_FLAG_SKIP_MULTICAST |
-        GAA_FLAG_SKIP_DNS_SERVER |
-        GAA_FLAG_SKIP_FRIENDLY_NAME,
-      NULL,
-      adapter_addresses,
-      &adapter_addresses_buffer_size);
-
-    if (ERROR_SUCCESS == error)
-    {
-      // We're done here, people!
-      break;
-    }
-    else if (ERROR_BUFFER_OVERFLOW == error)
-    {
-      // Try again with the new size
-      free(adapter_addresses);
-      adapter_addresses = NULL;
-
-      continue;
-    }
-    else
-    {
-      // Unexpected error code - log and throw
-      free(adapter_addresses);
-      adapter_addresses = NULL;
-
-      // @todo
-      LOG_AND_THROW_HERE();
-    }
-  }
-
-  // Iterate through all of the adapters
-  for (adapter = adapter_addresses; NULL != adapter; adapter = adapter->Next)
-  {
-    // Skip loopback adapters
-    if (IF_TYPE_SOFTWARE_LOOPBACK == adapter->IfType)
-    {
-      continue;
-    }
-
-    // Parse all IPv4 and IPv6 addresses
-    for (
-      IP_ADAPTER_UNICAST_ADDRESS* address = adapter->FirstUnicastAddress;
-      NULL != address;
-      address = address->Next)
-    {
-      auto family = address->Address.lpSockaddr->sa_family;
-      if (AF_INET == family)
-      {
-        // IPv4
-        SOCKADDR_IN* ipv4 = reinterpret_cast<SOCKADDR_IN*>(address->Address.lpSockaddr);
-
-        char str_buffer[INET_ADDRSTRLEN] = {0};
-        inet_ntop(AF_INET, &(ipv4->sin_addr), str_buffer, INET_ADDRSTRLEN);
-        ipAddrs.mIpv4.push_back(str_buffer);
-      }
-      else if (AF_INET6 == family)
-      {
-        // IPv6
-        SOCKADDR_IN6* ipv6 = reinterpret_cast<SOCKADDR_IN6*>(address->Address.lpSockaddr);
-
-        char str_buffer[INET6_ADDRSTRLEN] = {0};
-        inet_ntop(AF_INET6, &(ipv6->sin6_addr), str_buffer, INET6_ADDRSTRLEN);
-
-        std::string ipv6_str(str_buffer);
-
-        // Detect and skip non-external addresses
-        bool is_link_local(false);
-        bool is_special_use(false);
-
-        if (0 == ipv6_str.find("fe"))
-        {
-          char c = ipv6_str[2];
-          if (c == '8' || c == '9' || c == 'a' || c == 'b')
-          {
-            is_link_local = true;
-          }
-        }
-        else if (0 == ipv6_str.find("2001:0:"))
-        {
-          is_special_use = true;
-        }
-
-        if (! (is_link_local || is_special_use))
-        {
-          ipAddrs.mIpv6.push_back(ipv6_str);
-        }
-      }
-      else
-      {
-        // Skip all other types of addresses
-        continue;
-      }
-    }
-  }
-
-  // Cleanup
-  free(adapter_addresses);
-  adapter_addresses = NULL;
-
-  // Cheers!
-}
-*/
 # define snprintf _snprintf
   const char* inet_ntop(int af, const void* src, char* dst, int cnt) {
     struct sockaddr_storage sa;
@@ -260,7 +140,7 @@ class Pcap : public ObjectWrap {
       TryCatch try_catch;
       Handle<Value> emit_argv[3] = {
         packet_symbol,
-        Number::New(copy_len),
+        Number::New(static_cast<double>(copy_len)),
         Boolean::New(truncated)
       };
       obj->Emit->Call(obj->handle_, 3, emit_argv);
@@ -334,7 +214,8 @@ class Pcap : public ObjectWrap {
     static Handle<Value> Send(const Arguments& args) {
       HandleScope scope;
       Pcap *obj = ObjectWrap::Unwrap<Pcap>(args.This());
-      unsigned int buffer_size = 0;
+      //unsigned int buffer_size = 0;
+      size_t buffer_size = 0;
 
       if (args.Length() >= 1) {
         if (!Buffer::HasInstance(args[0])) {
@@ -380,7 +261,7 @@ class Pcap : public ObjectWrap {
 
       if (pcap_sendpacket(obj->pcap_handle,
                           (const u_char*)Buffer::Data(buffer_obj),
-                          buffer_size) == -1) {
+                          static_cast<int>(buffer_size)) == -1) {
         return ThrowException(
           Exception::Error(String::New(pcap_geterr(obj->pcap_handle)))
         );
@@ -793,3 +674,125 @@ extern "C" {
 
   NODE_MODULE(cap, init);
 }
+
+/*
+void ListIpAddresses(IpAddresses& ipAddrs)
+{
+  IP_ADAPTER_ADDRESSES* adapter_addresses(NULL);
+  IP_ADAPTER_ADDRESSES* adapter(NULL);
+
+  // Start with a 16 KB buffer and resize if needed -
+  // multiple attempts in case interfaces change while
+  // we are in the middle of querying them.
+  DWORD adapter_addresses_buffer_size = 16 * KB;
+  for (int attempts = 0; attempts != 3; ++attempts)
+  {
+    adapter_addresses = (IP_ADAPTER_ADDRESSES*)malloc(adapter_addresses_buffer_size);
+    assert(adapter_addresses);
+
+    DWORD error = ::GetAdaptersAddresses(
+      AF_UNSPEC,
+      GAA_FLAG_SKIP_ANYCAST |
+        GAA_FLAG_SKIP_MULTICAST |
+        GAA_FLAG_SKIP_DNS_SERVER |
+        GAA_FLAG_SKIP_FRIENDLY_NAME,
+      NULL,
+      adapter_addresses,
+      &adapter_addresses_buffer_size);
+
+    if (ERROR_SUCCESS == error)
+    {
+      // We're done here, people!
+      break;
+    }
+    else if (ERROR_BUFFER_OVERFLOW == error)
+    {
+      // Try again with the new size
+      free(adapter_addresses);
+      adapter_addresses = NULL;
+
+      continue;
+    }
+    else
+    {
+      // Unexpected error code - log and throw
+      free(adapter_addresses);
+      adapter_addresses = NULL;
+
+      // @todo
+      LOG_AND_THROW_HERE();
+    }
+  }
+
+  // Iterate through all of the adapters
+  for (adapter = adapter_addresses; NULL != adapter; adapter = adapter->Next)
+  {
+    // Skip loopback adapters
+    if (IF_TYPE_SOFTWARE_LOOPBACK == adapter->IfType)
+    {
+      continue;
+    }
+
+    // Parse all IPv4 and IPv6 addresses
+    for (
+      IP_ADAPTER_UNICAST_ADDRESS* address = adapter->FirstUnicastAddress;
+      NULL != address;
+      address = address->Next)
+    {
+      auto family = address->Address.lpSockaddr->sa_family;
+      if (AF_INET == family)
+      {
+        // IPv4
+        SOCKADDR_IN* ipv4 = reinterpret_cast<SOCKADDR_IN*>(address->Address.lpSockaddr);
+
+        char str_buffer[INET_ADDRSTRLEN] = {0};
+        inet_ntop(AF_INET, &(ipv4->sin_addr), str_buffer, INET_ADDRSTRLEN);
+        ipAddrs.mIpv4.push_back(str_buffer);
+      }
+      else if (AF_INET6 == family)
+      {
+        // IPv6
+        SOCKADDR_IN6* ipv6 = reinterpret_cast<SOCKADDR_IN6*>(address->Address.lpSockaddr);
+
+        char str_buffer[INET6_ADDRSTRLEN] = {0};
+        inet_ntop(AF_INET6, &(ipv6->sin6_addr), str_buffer, INET6_ADDRSTRLEN);
+
+        std::string ipv6_str(str_buffer);
+
+        // Detect and skip non-external addresses
+        bool is_link_local(false);
+        bool is_special_use(false);
+
+        if (0 == ipv6_str.find("fe"))
+        {
+          char c = ipv6_str[2];
+          if (c == '8' || c == '9' || c == 'a' || c == 'b')
+          {
+            is_link_local = true;
+          }
+        }
+        else if (0 == ipv6_str.find("2001:0:"))
+        {
+          is_special_use = true;
+        }
+
+        if (! (is_link_local || is_special_use))
+        {
+          ipAddrs.mIpv6.push_back(ipv6_str);
+        }
+      }
+      else
+      {
+        // Skip all other types of addresses
+        continue;
+      }
+    }
+  }
+
+  // Cleanup
+  free(adapter_addresses);
+  adapter_addresses = NULL;
+
+  // Cheers!
+}
+*/
